36319f6e16eeac4d8a347d6b820b2550ef608e0f
Rivindhu <73774880+Rivindhu@users.noreply.github.com>
2022-11-02 14:27:44 +0530

    modified app js file.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const appRoot = require("app-root-path");
-const config = require(appRoot + "./config/config.js");
+const config = require(appRoot + "/config/config.js");
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
